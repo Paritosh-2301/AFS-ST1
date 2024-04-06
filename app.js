9d715813b27d199fa6eebf0986f17092bb6ee3a2
Paritosh-2301 <paritoshpaul2301@gmail.com>
2024-04-06 22:47:23 +0530

    st1 commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const nodemailer= require('nodemailer');
+require("dotenv").config();
+function mailmeotp(userEmail){
+    const transporter=nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        service: 'gmail',
+        port: 465,
+        secure: true,
+        auth:{
+            user: "nishantjain409@gmail.com",
+            pass: process.env.PASS
+        }
+    })
+    
+    function makeMyOTP(){
+        let otp=Math.floor(100000 + Math.random() * 900000);
+        return otp.toString();
+    }
+    const newOTP=makeMyOTP(6);
+    const mail={
+        from: "nishantjain409@gmail.com",
+        to: userEmail,
+        subject: "Verify Your Account",
+        text: "here is your otp to verify your sign in on myApp: "+newOTP
+    }
+    
+    transporter.sendMail(mail,(error,info)=>{
+        if(error) {
+            console.log("Error");
+        }
+        else{
+            console.log("email sent: "+ info.response);
+        }
+    });
+    return newOTP;
+}
+
+module.exports=mailmeotp;
